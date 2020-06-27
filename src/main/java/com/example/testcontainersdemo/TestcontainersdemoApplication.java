ac96d75e674ee908179f767710cff1c01c854b93
Keshav Baweja <keshavbaweja@gmail.com>
2020-06-27 19:37:39 +0800

    Enable caching

--- src/main/java/com/example/testcontainersdemo/TestcontainersdemoApplication.java
+++ src/main/java/com/example/testcontainersdemo/TestcontainersdemoApplication.java
@@ -1,18 +1,26 @@
 package com.example.testcontainersdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @EnableJdbcRepositories("com.example.testcontainersdemo.repository")
+@EnableCaching
 public class TestcontainersdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestcontainersdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestcontainersdemoApplication.class, args);
+    }
+
+    @Value("${embedded.redis.host}")
+    private String redisHost;
+
+    @Value("${embedded.redis.port}")
+    private String redisPort;
 
 }
